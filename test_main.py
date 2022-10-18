954208bbe5ccf9e0a02c13b73825c3dba5841e47
Sudarshan Pakrashi <sudarshanpakrashi@ZT-Mac-IN-sudarshanpakrashi.local>
2022-10-18 22:24:51 +0530

    lint correction

--- test_main.py
+++ test_main.py
@@ -7,10 +7,10 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
 
-def test_read_main():
+def test_read_search():
     response = client.get("/search/Barack")
     assert response.status_code == 200
 
-def test_read_main():
+def test_read_wiki():
     response = client.get("/wiki/Barack Obama/10")
     assert response.status_code == 200
